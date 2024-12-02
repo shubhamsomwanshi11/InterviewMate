8dccc7299f7339a06e7b9e76bc181fe82db92f41
shubhamsomwanshi11 <shubhamsomwanshi11@gmail.com>
2024-12-02 10:01:01 +0530

    Updated Model

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 dotenv.config(); // Load environment variables from .env file
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+model = genAI.getGenerativeModel({ model: "gemini-1.5-pro-latest" });
 
 const app = express();
 const port = process.env.PORT || 3000;
